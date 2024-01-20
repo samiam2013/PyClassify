47187f25759cc0ba19740e66c68caf60c98f392f
Samuel Myres <samiam2013@gmail.com>
2024-01-20 12:04:48 -0800

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # PyClassify
 Python TensorFlow ResNet50 HTTP image classification server
+
+This is a result of Google's TensorFlow team no longer supporting Go, a programming language made with their own (El Goog's) money.
+
+Because I want to write my crawlers, which require image classification, in Go, but Python is the only Tensorflow supported language I really know, I have to do this somewhere, but my primary server for the site this is operating in service of _doesn't even have enough ram to load tensorflow_.
+
+This is not built with any Python Web Framework, instead relying on the very basic http.server 
+it expects a POST request to the endpoint `/classify` with a JSON-encoded JPEG image  
+
+```json
+{"image": "data:image/jpeg;base64,iVBORw0KGgoAAA...}
+```
+
+with or without the `data:image/jpeg;base64,`, that's just something my JS wanted to add.
+
+
+it will respond with something like 
+```
+{
+    "success": true,
+    "upload_size": 1,
+    "classifications": {
+        "television": "0.85180295",
+        "desktop_computer": "0.09160688",
+        "monitor": "0.04095296",
+        "screen": "0.006704196",
+        "notebook": "0.0039336607",
+        "home_theater": "0.002433881",
+        "iPod": "0.0011253094",
+        "entertainment_center": "0.00035410884",
+        "web_site": "0.00026599315",
+        "desk": "0.00025780668"
+    }
+}
+```
+
+always the top 10 classifications and all their weights or some kind of error.
---
+++ main.py
@@ -0,0 +1,127 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import base64
+import time
+import os
+import numpy as np
+
+print("Loading tensorflow...")
+# TF freaks about lack of cuda gpu without this
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+print("Tensorflow loaded.")
+
+print("Loading ResNet50 model...")
+# Load the ResNet50 model pre-trained on ImageNet data globally
+model = ResNet50(weights='imagenet')
+# Compile the model
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+print("ResNet50 model loaded.")
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            # get ./submit.py and send it back  
+            with open("./submit.html", "r") as f:
+                self.wfile.write(bytes(f.read(), "utf-8"))
+        except Exception as e:
+            error(self, 500, str(e))
+
+    def do_POST(self):
+        try:
+            # first confirm that the request is for /classify 
+            if self.path != "/classify": 
+                error_json(self, 400, "Bad request, must POST to /classify")
+            content_length = int(self.headers['Content-Length'])
+            data_input = self.rfile.read(content_length)
+            # decode from json to get image
+            data_input = json.loads(data_input.decode("utf-8"))
+            image = data_input.get("image")
+            if image is None:
+                error_json(self, 400, "Bad request, must include 'image' (b64 jpeg)")
+
+            # strip the MIME type and base64 designation from the multipart string then try to save
+            # "data:image/jpeg;base64,/9j/4AAQS....
+            if image.startswith("data:image/jpeg;base64,"):
+                image = image[len("data:image/jpeg;base64,"):]
+            
+            # get the current time + .jpg save the image
+            filename = str(time.time()) + ".jpg"
+            with open(filename, "wb") as fh:
+                fh.write(base64.b64decode(image))
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            classes = get_image_classes(filename)
+            if classes is None:
+                fh.close()
+                error_json(self, 500, "Internal server error")
+            response = {
+                "success": True,
+                "upload_size": len(data_input),
+                "classifications": classes
+            }
+            json_response = json.dumps(response)
+            self.wfile.write(bytes(json_response, "utf-8"))
+        except Exception as e:
+            error_json(self, 500, str(e))
+
+def get_image_classes(img_path):
+    # this is where we need to take that file handle and run it through our model
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+
+    prediction = model.predict(img_array,verbose=0)
+    preds = decode_predictions(prediction, top=10)
+
+    top_labels = []
+    label_probs = dict()
+    for pred in preds[0]:
+        top_labels.append(pred[1])
+        label_probs[pred[1]] = str(pred[2])
+    return label_probs
+
+
+def error(server, code, msg):
+    server.send_response(code)
+    server.send_header("Content-type", "text/html")
+    server.end_headers()
+    server.wfile.write(bytes("<html><head><title>classify server</title></head>", "utf-8"))
+    server.wfile.write(bytes("<body><p>%s</p></body></html>" % msg, "utf-8"))
+
+def error_json(server, code, msg):
+    server.send_response(code)
+    server.send_header("Content-type", "application/json")
+    server.end_headers()
+    response = {
+        "success": False,
+        "error": msg
+    }
+    json_response = json.dumps(response)
+    server.wfile.write(bytes(json_response, "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
